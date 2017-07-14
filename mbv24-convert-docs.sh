a32383c584cc01d483ef491eef181fb492455e83
mbv24 <mbv24@cornell.edu>
2017-07-14 12:38:09 -0400

    changed mbv24-convert-docs.sh

--- mbv24-convert-docs.sh
+++ mbv24-convert-docs.sh
@@ -6,10 +6,10 @@ OUTPUTFILE=$INPUTFILE
 # Convert markdown to HTML
 pandoc -o $OUTPUTFILE.html $INPUTFILE
 # Convert markdown to DOCX
-
+pandoc -o $OUTPUTFILE.docx $INPUTFILE
 # Convert markdown to ODT
-
+pandoc -o $OUTPUTFILE.odt $INPUTFILE
 # Convert markdown to PDF
-
+pandoc -o $OUTPUTFILE.pdf $INPUTFILE
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
 echo "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF"
