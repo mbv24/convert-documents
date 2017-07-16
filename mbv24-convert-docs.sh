63b0878d67a65ca965bd72b75cf53d36be73c895
mbv24 <mbv24@cornell.edu>
2017-07-16 13:42:24 -0400

    added portfolio.md and changed covert-docs.sh

--- mbv24-convert-docs.sh
+++ mbv24-convert-docs.sh
@@ -4,12 +4,19 @@
 INPUTFILE=$1
 OUTPUTFILE=$INPUTFILE
 # Convert markdown to HTML
+echo "Converting to html"
 pandoc -o $OUTPUTFILE.html $INPUTFILE
 # Convert markdown to DOCX
+echo "Converting to docx"
 pandoc -o $OUTPUTFILE.docx $INPUTFILE
 # Convert markdown to ODT
+echo "Converting to odt"
 pandoc -o $OUTPUTFILE.odt $INPUTFILE
 # Convert markdown to PDF
-pandoc -o $OUTPUTFILE.pdf $INPUTFILE
+echo "Converting to pdf"
+pandoc --latex-engine-pdflatex -o $OUTPUTFILE.pdf $INPUTFILE
+# Convert markdown to RTF
+echo "Converting to rtf"
+pandoc -o $OUTPUTFILE.rtf $INPUTFILE 
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
 echo "Converted $INPUTFILE to HTML, DOCX, ODT, PDF"
